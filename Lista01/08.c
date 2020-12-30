538beae867d5a19554a0470b658612931e6f7df5
Milena Carecho <37448340+MilenaCarecho@users.noreply.github.com>
2020-12-30 03:48:42 -0300

    Update 08.c

--- Lista01/08.c
+++ Lista01/08.c
@@ -3,6 +3,12 @@
 
 #include <stdio.h>
 int main () {
+    int n,a,s;
+    printf("Insira um numero inteiro: ");
+    scanf("%d", &n);
+    a=n-1;
+    s=n+1;
+    printf("Numero:%d \nAntecessor:%d \nSucessor:%d", n,a,s);
 
 }
 
