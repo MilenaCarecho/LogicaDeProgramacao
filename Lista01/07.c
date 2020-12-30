44ef1d5e928f094c7b909d8ffdd90aab30782e94
Milena Carecho <37448340+MilenaCarecho@users.noreply.github.com>
2020-12-30 03:44:42 -0300

    Update 07.c

--- Lista01/07.c
+++ Lista01/07.c
@@ -3,7 +3,13 @@ dólares **/
 
 
 #include <stdio.h>
+#include <math.h>
 int main () {
+    float dol, reais;
+    printf("Insira o valor em reais R$");
+    scanf("%f", &reais);
+    dol=reais/5.21;
+    printf("\nR$%.2f equivale a $%.2f", reais, dol);
 
 }
 
