f945b4604520f47184574b19b564836af9e1f0f2
Milena Carecho <37448340+MilenaCarecho@users.noreply.github.com>
2020-12-31 04:04:24 -0300

    Create 20.c

---
+++ C/20.c
@@ -0,0 +1,13 @@
+/** Escreva um algoritmo para criar um programa de ajuda para vendedores. A partir de um
+valor total lido, mostre:
+ O total a pagar com desconto de 10%;
+ O valor de cada parcela, no parcelamento de 3 x sem juros;
+ A comissão do vendedor, no caso de a venda ser a vista (5% sobre o valor com desconto)
+ A comissão do vendedor, no caso de a venda ser parcelada (5% sobre o valor total) **/
+
+
+#include <stdio.h>
+int main () {
+
+}
+
