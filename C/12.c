cca8bccb3ee6614cce29f6bbab35a9318e1ce677
Milena Carecho <37448340+MilenaCarecho@users.noreply.github.com>
2020-12-31 04:03:54 -0300

    Create 12.c

---
+++ C/12.c
@@ -0,0 +1,12 @@
+/** Sejam 𝑎 e 𝑏 os catetos de um triângulo, onde a hipotenusa é obtida pela equação:
+ℎ𝑖𝑝𝑜𝑡𝑒𝑛𝑢𝑠𝑎 = √𝑎
+2 + 𝑏
+2. Faça um programa que receba os valores de 𝑎 e 𝑏 e encontre o
+valor da hipotenusa através da equação. Imprima no final o resultado dessa operação. **/
+
+
+#include <stdio.h>
+int main () {
+
+}
+
