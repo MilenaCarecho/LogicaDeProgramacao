0c373c1d2f865664737049de1db059903fa3562d
Milena Carecho <37448340+MilenaCarecho@users.noreply.github.com>
2020-12-31 04:04:22 -0300

    Create 19.c

---
+++ C/19.c
@@ -0,0 +1,10 @@
+/** Receba o salário-base de um funcionário, calcule e imprima o salário a receber, sabendo-se
+que esse funcionário tem uma gratificação de 5% sobre o salário-base. Além disso, paga 7% de
+imposto sobre o salário-base **/
+
+
+#include <stdio.h>
+int main () {
+
+}
+
