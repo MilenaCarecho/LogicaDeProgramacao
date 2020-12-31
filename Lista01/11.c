137b39e82b3b081a1a05a5fd99b84f283d168391
Milena Carecho <37448340+MilenaCarecho@users.noreply.github.com>
2020-12-31 04:06:44 -0300

    Delete 11.c

--- Lista01/11.c
+++
@@ -1,9 +0,0 @@
-/** Leia o valor do raio de um círculo e calcule a área do círculo correspondente. Imprima o
-resultado dessa operação. A área do círculo é 𝜋 ∗ 𝑟𝑎𝑖𝑜², considere 𝜋 = 3,141592 . **/
-
-
-#include <stdio.h>
-int main () {
-
-}
-
